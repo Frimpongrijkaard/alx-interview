79cc37b0d93eec9c1e316c43de5a1679e002f8a1
Frimpong rijkaard <frimpongrijkaard94@yahoo.com>
2024-10-07 02:47:46 +0000

    change all comment to doc string

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
+"""A script to determine pascal's triangle for any number"""
+
 def pascal_triangle(n):
-    if n <= 0:
-        return []
-    
-    # Initialize the triangle as an empty list
+    """
+    returns a list of lists of integers representing the Pascal’s triangle of n
+    """
     pascal = []
+
+    if n <= 0:
+        return pascal
     
-    # Use a loop to build the triangle row by row
     for i in range(n):
-        
-        # List comprehension to generate each row
         
         row = [1 if j == 0 or j == i else pascal[i-1][j-1] + pascal[i-1][j] for j in range(i + 1)]
         
-        pascal.append(row)  # Append the row to the triangle
+        pascal.append(row)  
     
     return pascal
 
