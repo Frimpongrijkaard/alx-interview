e82d310f57abcbd73265ec18a1f7d06efa23bcdd
Frimpong rijkaard <frimpongrijkaard94@yahoo.com>
2024-10-25 18:16:20 +0000

    working fine but need more checked to pass this code

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,77 +4,43 @@
 """
 
 import sys
-import signal
 
 
-status_codes_count = {
-    "200": 0,
-    "301": 0,
-    "400": 0,
-    "401": 0,
-    "403": 0,
-    "404": 0,
-    "405": 0,
-    "500": 0
-}
+i = 0
+sum_file_size = 0
+status_code = {'200': 0,
+               '301': 0,
+               '400': 0,
+               '401': 0,
+               '403': 0,
+               '404': 0,
+               '405': 0,
+               '500': 0}
 
-
-total_file_size = 0
-line_count = 0
-
-def print_stats():
-    """Print the accumulated statistics."""
-    print("File size: {}".format(total_file_size))
-    for code in sorted(status_codes_count.keys()):
-        if status_codes_count[code] > 0:
-            print("{}: {}".format(code, status_codes_count[code]))
-
-def process_line(line):
-    """Process a single line of log data."""
-    global total_file_size, line_count
-
-    try:
-        
-        parts = line.split()
-
-        
-        if len(parts) < 7:
-            return  
-
-    
-        status_code = parts[8]
-        file_size = parts[9]
-
-        
-        try:
-            total_file_size += int(file_size)
-        except ValueError:
-            pass  
-
-        
-        if status_code in status_codes_count:
-            status_codes_count[status_code] += 1
-
-        line_count += 1
-
-    except IndexError:
-        pass  
-
-def handle_signal(signal, frame):
-    """Handle the CTRL + C signal to print stats and exit."""
-    print_stats()
-    sys.exit(0)
-
-signal.signal(signal.SIGINT, handle_signal)
-
-if __name__ == "__main__":
-    try:
-        for line in sys.stdin:
-            process_line(line)
-
-            if line_count % 10 == 0:
-                print_stats()
-
-    except KeyboardInterrupt:
-        print_stats()
-        sys.exit(0)
+try:
+    for line in sys.stdin:
+        args = line.split(' ')
+        if len(args) > 2:
+            status_line = args[-2]
+            file_size = args[-1]
+            if status_line in status_code:
+                status_code[status_line] += 1
+            sum_file_size += int(file_size)
+            i += 1
+            if i == 10:
+                print('File size: {:d}'.format(sum_file_size))
+                sorted_keys = sorted(status_code.keys())
+                for key in sorted_keys:
+                    value = status_code[key]
+                    if value != 0:
+                        print('{}: {}'.format(key, value))
+                i = 0
+except Exception:
+    pass
+finally:
+    print('File size: {:d}'.format(sum_file_size))
+    sorted_keys = sorted(status_code.keys())
+    for key in sorted_keys:
+        value = status_code[key]
+        if value != 0:
+            print('{}: {}'.format(key, value))
