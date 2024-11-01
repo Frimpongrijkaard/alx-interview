826696f68c7097a0e906600a78abc9189ac50bd2
Frimpong rijkaard <frimpongrijkaard94@yahoo.com>
2024-11-01 00:26:29 +0000

    solution to utf-8

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3 
+"""UTF-8 Validation"""
+
+def validUTF8(data):
+    """a method that determines if a given data set represents a valid UTF-8 encoding."""
+    n_bytes = 0
+
+    for num in data:
+        byte = num & 0xFF
+
+        if n_bytes == 0:
+            
+            if (byte >> 5) == 0b110:  
+                n_bytes = 1
+            elif (byte >> 4) == 0b1110:  
+                n_bytes = 2
+            elif (byte >> 3) == 0b11110:  
+                n_bytes = 3
+            elif (byte >> 7) != 0:  
+                return False
+        else:
+            
+            if (byte >> 6) != 0b10:
+                return False
+            n_bytes -= 1
+
+    
+    return n_bytes == 0
